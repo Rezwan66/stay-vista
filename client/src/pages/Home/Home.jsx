bccea8b386a8fbbb2e830f8c3255b9a45dd81cb6
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-23 08:04:54 +0600

    displayed categories on the homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
+import Categories from '../../components/Rooms/Categories/Categories';
 import Rooms from '../../components/Rooms/Rooms';
 
 const Home = () => {
   return (
     <div>
       {/* categories section */}
-
+      <Categories />
       {/* rooms section */}
       <Rooms />
     </div>
