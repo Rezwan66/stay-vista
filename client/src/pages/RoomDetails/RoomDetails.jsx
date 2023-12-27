252ac820c6aa3c08bd98db69e2deba4f100da72a
Rezwan66 <shaikhrezwan66@gmail.com>
2023-12-28 02:39:26 +0600

    displayed dynamic header title of page using react helmet- on Home and RoomDetails page

--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import Container from '../../components/Shared/Container';
 import { useEffect, useState } from 'react';
 import Loader from '../../components/Shared/Loader';
+import { Helmet } from 'react-helmet-async';
 
 const RoomDetails = () => {
   const [room, setRoom] = useState({});
@@ -24,7 +25,9 @@ const RoomDetails = () => {
 
   return (
     <Container>
-      <h1>{room.title}</h1>
+      <Helmet>
+        <title>{room?.title}</title>
+      </Helmet>
     </Container>
   );
 };
