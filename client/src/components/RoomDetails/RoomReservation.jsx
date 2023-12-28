032aa99dad38ff0e53e2354608297de3f17e01e0
Rezwan66 <shaikhrezwan66@gmail.com>
2023-12-28 18:34:49 +0600

    added room info and room reservation components to room details page

---
+++ client/src/components/RoomDetails/RoomReservation.jsx
@@ -0,0 +1,33 @@
+import { formatDistance } from 'date-fns';
+import Button from '../Button/Button';
+import Calender from './Calender';
+
+const RoomReservation = ({ room }) => {
+  // Price calculation: Total days * price
+  const totalDays = parseInt(
+    formatDistance(new Date(room?.to), new Date(room?.from)).split(' ')[0]
+  );
+  const totalPrice = totalDays * room?.price;
+  return (
+    <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
+      <div className="flex items-center gap-1 p-4">
+        <div className="text-2xl font-bold">$ {room?.price}</div>
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+      <div className="flex justify-center">
+        <Calender />
+      </div>
+      <hr />
+      <div className="p-4">
+        <Button label={'Reserve'} />
+      </div>
+      <hr />
+      <div className="p-4 flex items-center justify-between font-semibold text-lg">
+        <div>Total</div>
+        <div>$ {totalPrice}</div>
+      </div>
+    </div>
+  );
+};
+export default RoomReservation;
