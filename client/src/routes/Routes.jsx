a722b9fa784cbdc7a6025c13c05130bee1a7ff26
Rezwan66 <shaikhrezwan66@gmail.com>
2024-01-07 22:29:34 +0600

    implemented common profile route with Profile component on dashboard

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -13,6 +13,7 @@ import MyListings from '../pages/Dashboard/Host/MyListings';
 import HostRoute from './HostRoute';
 import AdminRoute from './AdminRoute';
 import ManageUsers from '../pages/Dashboard/Admin/ManageUsers';
+import Profile from '../pages/Dashboard/Common/Profile';
 
 export const router = createBrowserRouter([
   {
@@ -45,6 +46,14 @@ export const router = createBrowserRouter([
       </PrivateRoute>
     ),
     children: [
+      {
+        path: 'profile',
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        ),
+      },
       // host routes
       {
         path: 'add-room',
